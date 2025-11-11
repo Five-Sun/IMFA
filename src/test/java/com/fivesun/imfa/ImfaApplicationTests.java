031cf8b193791b13b15133a42d13d5c5e9a55a5c
five-sun <tyo1012@naver.com>
2025-11-12 00:29:01 +0900

    init commit

---
+++ src/test/java/com/fivesun/imfa/ImfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivesun.imfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
